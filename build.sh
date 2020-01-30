772bb643864ccaf51feb3b772fbe985a827fb018
Vlad Wolanyk <vlad@himama.com>
2020-01-30 11:35:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # set -x
 set -e
 
-docker build --cache-from=imagemagick-heif-heroku18 -t imagemagick-heif-heroku18 .
+docker build --cache-from=imagemagick-heif-heroku16 -t imagemagick-heif-heroku16 .
 # docker build --no-cache -t imagemagick-heif-heroku18 .
 mkdir -p build
 
-docker run --rm -t -v $PWD/build:/data imagemagick-heif-heroku18 sh -c 'cp -f /usr/src/imagemagick/build/*.tar.gz /data/'
+docker run --rm -t -v $PWD/build:/data imagemagick-heif-heroku16 sh -c 'cp -f /usr/src/imagemagick/build/*.tar.gz /data/'
